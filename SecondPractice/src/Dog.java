faf79c73fe9492635127fbf4587b011739db2edd
Veronika Pasechnik <paveronika@gmail.com>
2016-03-29 23:48:04 +0300

    Second version

--- SecondPractice/src/Dog.java
+++ SecondPractice/src/Dog.java
@@ -1,62 +1,86 @@
 import java.util.Scanner;
 
 public class Dog {
-	String name;
-	int age;
-	String size;
-	
-	public Dog(){
-		this.name = Generator.generateName();
-		this.age = Generator.generateAge();
-		this.size = Generator.generateSize();
-	}
-	
-	public Dog(String name, int age, String size){
-		this.name = name;
-		this.age = age;
-		this.size = size;
-	}
-	
-	public Dog(String name, int age){
-		this.name = name;
-		this.age = age;
-		this.size = Generator.generateSize();
-	}
-	
-	public Dog(String name){
-		this.name = name;
-		this.age = Generator.generateAge();
-		this.size = Generator.generateSize();
-	}
-	
-	
-	public String toString(){
-		String result;
-		result = "Dog parametres: ";
-		result += "name - " + name + ", ";
-		result += "age - " + age + ", ";
-		result += "size - " + size;
-		return result;
-	}
-	
+        String name;        
+        String size;
+        int age;
 
-	public static void main(String[] args) {
-					
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter amount of dogs and their names devided by space: "); 
-		String inputString = sc.nextLine();
-		String[] words = inputString.split("\\s+");
-		int firstElem = new Integer(words[0]);		
-		Dog dogs[] = new Dog[firstElem];
-		for (int i = 0; i < firstElem; i++) 
-			if (i < words.length - 1)
-				dogs[i] = new Dog(words[i + 1]);
-			else
-				dogs[i] = new Dog(); 
-		
-		for (int i = 0; i < firstElem; i++)
-	    	System.out.println(dogs[i] + " ");
+        public Dog(){
+                this.name = Generator.generateName();                
+                this.size = Generator.generateSize();
+                this.age = Generator.generateAge();
+        }
 
-	}	
+        public Dog(String name, String size, int age){
+        	if (name == null)
+        		this.name = Generator.generateName();
+        	else
+                this.name = name;        	
+
+        	if (size == null)
+        		this.size = Generator.generateSize();
+        	else
+                this.size = size;
+        	// Bad check: 0 is valid age
+        	if (age == 0)
+        		this.age = Generator.generateAge();
+        	else
+                this.age = age;        	
+        }
+        
+
+        public String toString(){
+                String result;
+                result = "Dog parametres: ";
+                result += "name - " + name + ", ";
+                result += "age - " + age + ", ";
+                result += "size - " + size;
+                return result;
+        }
+
+
+        public static void main(String[] args) {
+
+                Scanner sc = new Scanner(System.in);
+                
+                System.out.print("Enter amount of dogs and their names devided by space: ");
+                String inputString = sc.nextLine();
+                String[] words = inputString.split("\\s+");
+                int numDogs = Integer.parseInt(words[0]);
+                String[] names = new String[numDogs];
+                for (int i = 0; i < numDogs; i++)
+                	if (i + 1 < words.length)
+                		names[i] = words[i + 1];
+                
+                System.out.print("Enter sizes of dogs (small, medium, big) devided by space: ");
+                inputString = sc.nextLine();
+                words = inputString.split("\\s+");
+                String[] sizes = new String[numDogs];
+                for (int i = 0; i < numDogs; i++)
+                	// If empty string were entered words length still be 1
+                	if (i < words.length)
+                		sizes[i] = words[i];
+                
+                System.out.print("Enter age of dogs devided by space: ");
+                inputString = sc.nextLine();
+                words = inputString.split("\\s+");
+                int[] ages = new int[numDogs];
+                
+                for (int i = 0; i < numDogs; i++)
+                	// If empty string were entered words length still be 1
+                	if (i < words.length)
+                		ages[i] = Integer.parseInt(words[i]);
+                
+                Dog dogs[] = new Dog[numDogs];
+                
+                for (int i = 0; i < numDogs; i++)                 	
+                	dogs[i] = new Dog(names[i], sizes[i], ages[i]);                
+        
+                for (int i = 0; i < numDogs; i++)
+                        System.out.println(dogs[i] + " ");
+                
+                sc.close();
+                
+        }
 
 }

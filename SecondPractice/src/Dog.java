53dd454209a7bb4d6fcfaefbc499396549781c8f
veronika.pasechnik <veronika.pasechnik>
2016-04-13 12:32:20 +0300

    no message

--- SecondPractice/src/Dog.java
+++ SecondPractice/src/Dog.java
@@ -5,7 +5,6 @@ public class Dog {
         String name;        
         String size;
         int age;
-
         public Dog(){
                 this.name = Generator.generateName();                
                 this.size = Generator.generateSize();

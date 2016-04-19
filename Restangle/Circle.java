b773cc4da80bf5cab37e5cc4c76073f9051e61c1
veronika.pasechnik <veronika.pasechnik>
2016-04-19 18:47:57 +0300

    no message

--- Restangle/Circle.java
+++ Restangle/Circle.java
@@ -3,24 +3,5 @@ public class Circle {
 
 	int  circumference;
 	
-	public Circle (int sideA, int sideB){
-			this.sideA = sideA;
-			this.sideB = sideB;
-	}
-	
-	public Point(int x, int y){
-		this.x = x; this.y = y; 
-	}
-	
-	public double square(){
-		return this.sideA*this.sideB; 
-	}
-	
-	public double perimetr(){
-		return 2*(this.sideA+this.sideB);
-	}	
-	
-
-}
 
 }
